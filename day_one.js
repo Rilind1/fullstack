63e093db3f20ed421872e79760c5e4025c58a65e
Rilind Hoxhaj <rilind@Rilinds-MacBook-Pro.local>
2020-03-16 18:22:11 +0100

    Test

--- day_one.js
+++ day_one.js
@@ -1,63 +1,63 @@
-//1. isString
+1. isString
 
-// function isString(string){
-//     if(typeof string === 'string'){
-//       return true;
-//     } else{
-//       return false;
-//     }
-// }
+function isString(string){
+    if(typeof string === 'string'){
+      return true;
+    } else{
+      return false;
+    }
+}
 
-// console.log(isString(5))
+console.log(isString(5))
 
-///////////////////////////////////////////////////
+/////////////////////////////////////////////////
 
-//2. check for array
+2. check for array
 
-// function checkForArray(array){
-//   if(Array.isArray(array)){
-//         return true;
-//   } else{
-//     return false;
-//   }
-// }
+function checkForArray(array){
+  if(Array.isArray(array)){
+        return true;
+  } else{
+    return false;
+  }
+}
 
-// console.log(checkForArray({ a: 2 }))
+console.log(checkForArray({ a: 2 }))
 
-///////////////////////////////////////////////////
+/////////////////////////////////////////////////
 
-//3. Check for Types in Array, solved this googling, still do not understand it!!!
+3. Check for Types in Array, solved this googling, still do not understand it!!!
 
-// function checkType(array){
-//   return array.every(function(i)
-//   { return typeof i === "string" 
-// });
-// }
+function checkType(array){
+  return array.every(function(i)
+  { return typeof i === "string" 
+});
+}
 
-// console.log(checkType(['hello', 'rilind']))
+console.log(checkType(['hello', 'rilind']))
 
-//4. Sort and Remove Duplicated Characters, do not know this
-// let a = 'aabbccdd';
-// let b = 'ccddeeff';
-// let c = '';
+4. Sort and Remove Duplicated Characters, do not know this
+let a = 'aabbccdd';
+let b = 'ccddeeff';
+let c = '';
 
-//5. Convert Number to Reversed Array of Digits , needed a hand !!!
+5. Convert Number to Reversed Array of Digits , needed a hand !!!
 
-// function reverseArray(array){
-//   let arr = (""+array).split('');
-//   let reversedArray = [];
-//   for(let i = arr.length; i>=0; i--){
-//     reversedArray[i] = arr.shift();
-//   }
-//   return reversedArray;
-// }
+function reverseArray(array){
+  let arr = (""+array).split('');
+  let reversedArray = [];
+  for(let i = arr.length; i>=0; i--){
+    reversedArray[i] = arr.shift();
+  }
+  return reversedArray;
+}
 
-// console.log(reverseArray(34521));
+console.log(reverseArray(34521));
 
-//6. Count Repetitions
+6. Count Repetitions
 
 function count(){
   let authors = ['kerouac', 'fante', 'fante', 'buk', 'hemingway', 'hornby', 'kerouac', 'buk', 'fante'];
   for
-},
+}
 
